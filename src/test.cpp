ab4debb0f04a1abf09a31a20b1f6230f7dfc1d5b
Anna Scanu <ascanu@lxplus9109.cern.ch>
2025-10-21 16:15:58 +0200

    Tested a new way to plot histograms (added in test.cpp, soon to be merged into the main code)

--- src/test.cpp
+++ src/test.cpp
@@ -10,75 +10,6 @@
 
 // To compile: c++ DUNE_vs_T2K_plots.cpp `root-config --cflags --libs` -o plots.out
 
-// ------------------------------------------------------------------------------------------------
-//                Function to process one tree and fill histograms
-// ------------------------------------------------------------------------------------------------
-void ProcessTree(TTree* tree,
-                 TH1F* hEnu, TH1F* hDelta, TH1F* hDeltaWeighted,
-                 TH1F* hCCQE, TH1F* hRES, TH1F* h2p2h, TH1F* hOther,
-                 TH1F* hDeltaCCQE, TH1F* hDeltaRES, TH1F* hDelta2p2h, TH1F* hDeltaOther,
-                 bool isDUNE)
-{
-    int Mode;
-    Float_t Enu_true, Erecoil_minerva, ELep, Enu_QE;
-    bool flag_CCINC, flag_CC0pi;
-
-    tree->SetBranchAddress("Mode", &Mode);
-    tree->SetBranchAddress("Enu_true", &Enu_true);
-
-    if (isDUNE) 
-    {
-        tree->SetBranchAddress("Erecoil_minerva", &Erecoil_minerva);
-        tree->SetBranchAddress("ELep", &ELep);
-        tree->SetBranchAddress("flagCCINC", &flag_CCINC);
-    } else 
-    {
-        tree->SetBranchAddress("Enu_QE", &Enu_QE);
-        tree->SetBranchAddress("flagCC0pi", &flag_CC0pi);
-    }
-
-    for (Long64_t i = 0; i < tree->GetEntries(); i++) 
-    {
-        tree->GetEntry(i);
-
-        //if ((isDUNE && !flag_CCINC) || (!isDUNE && !flag_CC0pi)) // if it's DUNE and it's not CCINC, or T2K and not CC0pi...
-        //    continue; // ... skip the event (I just want CC-inclusive events for DUNE, or CC interaction with no true pions
-                      //  in the final state for T2K)
-
-        if (isDUNE && !flag_CCINC)
-            continue;   
-        if (!isDUNE && !flag_CC0pi)
-            continue;
-
-        float reco = isDUNE ? (Erecoil_minerva + ELep) : Enu_QE;
-        float diff = Enu_true - reco;
-
-        // Fill global histos
-        hEnu->Fill(Enu_true);
-        hDelta->Fill(diff);
-        hDeltaWeighted->Fill(diff / Enu_true);
-
-        // Mode-separated histos
-        if (Mode == 1) 
-        {
-            hCCQE->Fill(Enu_true);
-            hDeltaCCQE->Fill(diff);
-        } else if (Mode == 2) 
-        {
-            h2p2h->Fill(Enu_true);
-            hDelta2p2h->Fill(diff);
-        } else if (Mode == 11 || Mode == 12 || Mode == 13) 
-        {
-            hRES->Fill(Enu_true);
-            hDeltaRES->Fill(diff);
-        } else 
-        {
-            hOther->Fill(Enu_true);
-            hDeltaOther->Fill(diff);
-        }
-    }
-}
-
 int main(int argc, char ** argv) 
 {
     gStyle->SetOptStat(0);
@@ -128,14 +59,14 @@ int main(int argc, char ** argv)
     hFluxDUNE->SetTitle("DUNE neutrino flux;E_{#nu} [MeV];Unosc #nu_{#mu}/m^{2}/POT/GeV");
     c1->SetTitle("DUNE neutrino flux");
     hFluxDUNE->Draw("hist");
-    c1->SaveAs("../DUNE_T2K_Plots/DUNE_flux.pdf");
+    c1->SaveAs("../Test_new_plots/DUNE_flux.pdf");
 
     TCanvas *c1_T2K = new TCanvas("c1_T2K", "T2K flux", 800, 600);
     hFluxT2K->GetXaxis()->SetRangeUser(0, 9);
     hFluxT2K->SetTitle("T2K neutrino flux;E_{#nu} [MeV];Unosc #nu_{#mu}/m^{2}/POT/GeV");
     c1_T2K->SetTitle("T2K neutrino flux");
     hFluxT2K->Draw("hist");
-    c1_T2K->SaveAs("../DUNE_T2K_Plots/T2K_flux.pdf");
+    c1_T2K->SaveAs("../Test_new_plots/T2K_flux.pdf");
 
     // -------------------------------------------------------------------------------------------------------------
     //                                   Histogram definitions 
@@ -167,18 +98,45 @@ int main(int argc, char ** argv)
     TH1F *hT2K_Delta_2p2h  = new TH1F("hT2K_Delta_2p2h","T2K 2p2h DeltaE;E_{#nu}^{true} - E_{#nu}^{reco} [MeV];Entries", 50, -0.5, 2.5);
     TH1F *hT2K_Delta_Other = new TH1F("hT2K_Delta_Other","T2K Other DeltaE;E_{#nu}^{true} - E_{#nu}^{reco} [MeV];Entries", 50, -0.5, 2.5);
 
-    // ----------------------------------------------------------------------------------------------
-    //                           Process both trees (filling histograms)
-    // ----------------------------------------------------------------------------------------------
-    ProcessTree(tDUNE, hEnuDUNE, hDeltaDUNE, hDeltaDUNE_Weighted,
-                hDUNE_CCQE, hDUNE_RES, hDUNE_2p2h, hDUNE_Other,
-                hDUNE_Delta_CCQE, hDUNE_Delta_RES, hDUNE_Delta_2p2h, hDUNE_Delta_Other,
-                true);
+    // Testing another way to process trees. Example:
+    //TH1D* h_test = new TH1D("h_test_name");
+    //tree->Project("h_test_name", "Enu_QE/Enu_true", "flagCCINC*(Mode==2)", "hist")
 
-    ProcessTree(tT2K, hEnuT2K, hDeltaT2K, hDeltaT2K_Weighted,
-                hT2K_CCQE, hT2K_RES, hT2K_2p2h, hT2K_Other,
-                hT2K_Delta_CCQE, hT2K_Delta_RES, hT2K_Delta_2p2h, hT2K_Delta_Other,
-                false);
+    // True neutrino energy
+    tDUNE  -> Project("hEnuDUNE", "Enu_true", "flagCCINC", "hist");
+    tT2K   -> Project("hEnuT2K", "Enu_true", "flagCC0pi", "hist");
+
+    // Energy bias
+    tDUNE  -> Project("hDeltaDUNE", "Enu_true - (Erecoil_minerva+ELep)", "flagCCINC", "hist");
+    tT2K   -> Project("hDeltaT2K", "Enu_true - Enu_QE", "flagCC0pi", "hist");
+
+    // Weighted energy bias
+    tDUNE  -> Project("hDeltaDUNE_Weighted", "(Enu_true-(Erecoil_minerva+ELep))/Enu_true", "flagCCINC", "hist");
+    tT2K   -> Project("hDeltaT2K_Weighted", "(Enu_true-Enu_QE)/Enu_true", "flagCC0pi", "hist");
+
+    // DUNE Mode separation
+    tDUNE  -> Project("hDUNE_CCQE", "Enu_true", "flagCCINC * (Mode==1)", "hist"); // CCQE = Mode 1
+    tDUNE  -> Project("hDUNE_2p2h", "Enu_true", "flagCCINC * (Mode==2)", "hist"); // 2p2h = Mode 2 
+    tDUNE  -> Project("hDUNE_RES", "Enu_true", "flagCCINC && ( (Mode==11) || (Mode==12) || (Mode==13) )", "hist"); // RES = Mode 11, 12, 13
+    tDUNE  -> Project("hDUNE_Other", "Enu_true", "flagCCINC * (Mode!=1) * (Mode!=2) * (Mode!=11) * (Mode!=12) * (Mode!=13)", "hist"); // "Other"
+
+    // T2K Mode separation
+    tT2K   -> Project("hT2K_CCQE", "Enu_true", "flagCC0pi * (Mode==1)", "hist");
+    tT2K   -> Project("hT2K_2p2h", "Enu_true", "flagCC0pi * (Mode==2)", "hist");
+    tT2K   -> Project("hT2K_RES", "Enu_true", "flagCC0pi && ( (Mode==11) || (Mode==12) || (Mode==13) )", "hist");
+    tT2K   -> Project("hT2K_Other", "Enu_true", "flagCC0pi * (Mode!=1) * (Mode!=2) * (Mode!=11) * (Mode!=12) * (Mode!=13)", "hist");
+
+    // DUNE Energy bias by mode
+    tDUNE  -> Project("hDUNE_Delta_CCQE", "Enu_true-(Erecoil_minerva+ELep)", "flagCCINC * (Mode==1)", "hist"); // CCQE = Mode 1
+    tDUNE  -> Project("hDUNE_Delta_2p2h", "Enu_true-(Erecoil_minerva+ELep)", "flagCCINC * (Mode==2)", "hist"); // 2p2h = Mode 2 
+    tDUNE  -> Project("hDUNE_Delta_RES", "Enu_true-(Erecoil_minerva+ELep)", "flagCCINC && ( (Mode==11) || (Mode==12) || (Mode==13) )", "hist"); // RES = Mode 11, 12, 13
+    tDUNE  -> Project("hDUNE_Delta_Other", "Enu_true-(Erecoil_minerva+ELep)", "flagCCINC * (Mode!=1) * (Mode!=2) * (Mode!=11) * (Mode!=12) * (Mode!=13)", "hist"); // "Other"
+
+    // T2K Energy bias by mode
+    tT2K   -> Project("hT2K_Delta_CCQE", "Enu_true-Enu_QE", "flagCC0pi * (Mode==1)", "hist");
+    tT2K   -> Project("hT2K_Delta_2p2h", "Enu_true-Enu_QE", "flagCC0pi * (Mode==2)", "hist");
+    tT2K   -> Project("hT2K_Delta_RES", "Enu_true-Enu_QE", "flagCC0pi && ( (Mode==11) || (Mode==12) || (Mode==13) )", "hist");
+    tT2K   -> Project("hT2K_Delta_Other", "Enu_true-Enu_QE", "flagCC0pi * (Mode!=1) * (Mode!=2) * (Mode!=11) * (Mode!=12) * (Mode!=13)", "hist");
 
     // ----------------------------------------------------------------------------------------------
     //                                       Plotting
@@ -197,7 +155,7 @@ int main(int argc, char ** argv)
     leg2->AddEntry(hEnuT2K, "T2K", "l");
     leg2->Draw();
 
-    c2->SaveAs("../DUNE_T2K_Plots/true_energy_comparison.pdf");
+    c2->SaveAs("../Test_new_plots/true_energy_comparison.pdf");
 
     // DeltaE (E_true - E_reco) comparison
     hDeltaDUNE->SetLineColor(kRed);
@@ -212,7 +170,7 @@ int main(int argc, char ** argv)
     leg3->AddEntry(hDeltaT2K, "T2K", "l");
     leg3->Draw();
 
-    c3->SaveAs("../DUNE_T2K_Plots/delta_energy_comparison.pdf");
+    c3->SaveAs("../Test_new_plots/delta_energy_comparison.pdf");
 
     // Weighted DeltaE [(E_true - E_reco)/E_true] comparison
     hDeltaDUNE_Weighted->SetLineColor(kRed);
@@ -227,7 +185,7 @@ int main(int argc, char ** argv)
     leg3_2->AddEntry(hDeltaT2K_Weighted, "T2K", "l");
     leg3_2->Draw();
 
-    c3_2->SaveAs("../DUNE_T2K_Plots/delta_energy_comparison_weighted.pdf");
+    c3_2->SaveAs("../Test_new_plots/delta_energy_comparison_weighted.pdf");
 
     // Mode-separated DUNE plot
     hDUNE_CCQE->SetLineColor(kGreen+2);
@@ -248,7 +206,7 @@ int main(int argc, char ** argv)
     leg4->AddEntry(hDUNE_Other, "Other", "l");
     leg4->Draw();
 
-    c4->SaveAs("../DUNE_T2K_Plots/DUNE_modes.pdf");
+    c4->SaveAs("../Test_new_plots/DUNE_modes.pdf");
 
     // Mode-separated T2K plot
     hT2K_CCQE->SetLineColor(kGreen+2);
@@ -269,7 +227,7 @@ int main(int argc, char ** argv)
     leg5->AddEntry(hT2K_Other, "Other", "l");
     leg5->Draw();
 
-    c5->SaveAs("../DUNE_T2K_Plots/T2K_modes.pdf");
+    c5->SaveAs("../Test_new_plots/T2K_modes.pdf");
 
     // DUNE DeltaE (E_true - E_reco) by mode
     hDUNE_Delta_CCQE->SetLineColor(kGreen+2);
@@ -290,7 +248,7 @@ int main(int argc, char ** argv)
     leg6->AddEntry(hDUNE_Delta_Other, "Other", "l");
     leg6->Draw();
 
-    c6->SaveAs("../DUNE_T2K_Plots/DUNE_deltaE_modes.pdf");
+    c6->SaveAs("../Test_new_plots/DUNE_deltaE_modes.pdf");
 
     // T2K DeltaE (E_true - E_reco) by mode
     hT2K_Delta_CCQE->SetLineColor(kGreen+2);
@@ -312,7 +270,7 @@ int main(int argc, char ** argv)
     leg7->AddEntry(hT2K_Delta_Other, "Other", "l");
     leg7->Draw();
 
-    c7->SaveAs("../DUNE_T2K_Plots/T2K_deltaE_modes.pdf");
+    c7->SaveAs("../Test_new_plots/T2K_deltaE_modes.pdf");
 
     // ----------------------------------------------------------------------------------------------
     //                          Comparison DUNE & T2K separated by mode
@@ -351,7 +309,7 @@ int main(int argc, char ** argv)
     leg8->AddEntry(hT2K_Other, "T2K Other", "l");
     leg8->Draw();
 
-    c8->SaveAs("../DUNE_T2K_Plots/DUNE_T2K_modes_comparison.pdf");
+    c8->SaveAs("../Test_new_plots/DUNE_T2K_modes_comparison.pdf");
 
     // Close files
     file_DUNE->Close();
